c75ceac51d049fdcd87dc32518740eb3b63155fb
Kevin Nguyen <kevinngx625@gmail.com>
2023-03-11 19:18:43 +1100

    Week 4 Lectures

--- Assignments/Assignment 1/Assignment 1 [kevinngx]/perimeter.py
+++ Assignments/Assignment 1/Assignment 1 [kevinngx]/perimeter.py
@@ -1,14 +1,171 @@
 import sys
 
+class rectangle:
+    def __init__(self, x1, y1, x2, y2):
+        
+        self.x1 = int(x1)
+        self.y1 = int(y1)
+        self.x2 = int(x2)
+        self.y2 = int(y2)
+    
+    def print_points(self):
+        print(f'p1 = ({self.x1},{self.y1}) , p2 = ({self.x2},{self.y2})')
+    
+    def miny(self):
+        return min(self.y1, self.y2)
+    
+    def minx(self):
+        return min(self.x1, self.x2)
+    
+    def maxy(self):
+        return max(self.y1, self.y2)
+    
+    def maxx(self):
+        return max(self.x1, self.x2)
+        
+def print_all_rectangles(rectangles):
+    for rectangle in rectangles:
+        rectangle.print_points()
 
-# INSERT YOUR OWN FUNCTIONS
+def get_min_points():
+    minx = 0
+    miny = 0
+    for rectangle in rectangles:
+        minx = min(minx, rectangle.minx())
+        miny = min(miny, rectangle.miny())
+    return minx, miny    
+        
+def get_grid_shift():
+    # We only want to shift where the lowest points are negative. If they are greater than 0, we do not shift the points i.e. 0
+    minx, miny = get_min_points()
+    minx = min(minx, 0)
+    miny = min(miny, 0)
+    return abs(minx), abs(miny)
+
+def get_max_points(rectangles):
+    maxx = 0
+    maxy = 0
+    for rectangle in rectangles:
+        maxx = max(maxx, rectangle.maxx())
+        maxy = max(maxy, rectangle.maxy())
+    return maxx, maxy  
+    
+
+def shift_points(xshift, yshift):
+    for rectangle in rectangles:
+        rectangle.x1 += xshift + 1
+        rectangle.x2 += xshift + 1
+        rectangle.y1 += yshift + 1
+        rectangle.y2 += yshift + 1
+        
+def print_grid(grid):
+    y_cord = len(grid)
+    x_cord = len(grid[0])
+    
+    for y in range(len(grid)):
+        print(f'{y_cord:5}|',end='')
+        
+        for x in range(len(grid[0])):
+            print(f'{grid[-y][x]:3}', end='')
+        
+        y_cord = y_cord - 1
+        print()
+        
+    print(f'{0:5}|',end='')
+    print('---' * len(grid[0]))
+    print(f'     |',end='')
+    for i in range(len(grid[0])):
+        print(f'{i:3}', end='')
+
+def draw_rectangle(grid, rectangle):
+    minx = rectangle.minx()
+    maxx = rectangle.maxx()
+    miny = rectangle.miny()
+    maxy = rectangle.maxy()
+    for x in range(minx, maxx):
+        for y in range(miny, maxy):
+            if x in (minx, maxx-1) or y in (miny, maxy-1):
+                    grid[y][x] = '0'
+            else: 
+                grid[y][x] = '-'
+        
+def assign_perimeter_values(grid):
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[x][y] == '0':
+                grid[x][y] = check_surrounding(grid, x, y)
+                
+def check_surrounding(grid, x, y):
+    surrounding_perimeter = 0
+    try:
+        if grid[x][y+1] == '':
+            surrounding_perimeter += 1
+    except IndexError:
+        surrounding_perimeter += 1
+    try:
+        if grid[x][y-1] == '':
+            surrounding_perimeter += 1
+    except IndexError:
+        surrounding_perimeter += 1
+    
+    try:
+        if grid[x+1][y] == '':
+            surrounding_perimeter += 1
+    except IndexError:
+        surrounding_perimeter += 1
+    
+    try:
+        if grid[x-1][y] == '':
+            surrounding_perimeter += 1
+    except IndexError:
+        surrounding_perimeter += 1
+    
+    return surrounding_perimeter
+
+def count_perimeter(grid):
+    sum = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[x][y] not in ('', '-','0'):
+                sum += grid[x][y]
+    return sum
 
 filename = input('Which data file do you want to use? ')
+# filename = 'frames_1.txt'
+
+rectangles = []
+
 try:
     with open(filename) as file:
-        pass
-        # REPLACE PASS ABOVE WITH YOUR CODE
+
+        for line in file:
+            x1, y1, x2, y2 = line.removesuffix('\n').split(' ')
+            rectangles.append(rectangle(x1, y1, x2, y2))
+
+
 except FileNotFoundError:
     print('Could not open a file named', filename)
     print('Giving up...')
     sys.exit()
+
+# Step 1 = Shift the rectangles such that all points are positive (makes it easier to map to an array)
+xshift, yshift = get_grid_shift()
+shift_points(xshift, yshift)
+# print_all_rectangles(rectangles)
+
+# Step 2 Create an empty grid for us to draw on
+maxx, maxy = get_max_points(rectangles)
+grid = [[''] * maxx for _ in range(maxy)]
+# print_grid(grid)
+
+# Step 3 Draw on the grid  
+for rectangle in rectangles:
+    draw_rectangle(grid, rectangle)
+# print_grid(grid)
+
+# Step 4 Assign the perimeter value
+assign_perimeter_values(grid)
+# print_grid(grid)
+
+# Step 5 Calculate and print the perimeter
+print('The perimeter is:', count_perimeter(grid))
--- Assignments/Assignment 1/Assignment 1 [kevinngx]/poker_dice.py
+++ Assignments/Assignment 1/Assignment 1 [kevinngx]/poker_dice.py
@@ -1,13 +1,129 @@
-from random import randint
+from random import randint, seed
 
+cards = ['Ace', 'King', 'Queen', 'Jack', '10', '9']
 
-def play():
-    pass
-    # REPLACE PASS ABOVE WITH YOUR CODE
+def generate_hand(hand):
+    # Generate hand up to 5 cards / rolls
+    for i in range(5-len(hand)):
+        hand.append(cards[randint(0,5)])
+    hand = sorted(hand, key=cards.index)
+    return hand   
+    
+def identify_hand(hand):
+    # Identify the number of each cards rolled
+    hand_values = {card: 0 for card in cards} 
+    for card in hand:
+        hand_values[card] += 1
+    
+    # Sort the hands, and grab the two most common cards
+    sorted_hand = sorted(hand_values.items(), key=lambda item: item[1], reverse=True)
+    combo = sorted_hand[0][1], sorted_hand[1][1]
+    
+    # If there are no combos i.e. all cards occur just once then it is either a straight or a bust
+    # We know it's a straight if either 9 or Ace does not appear in the hand of 5
+    if combo == (1, 1): 
+        if hand_values['9'] == 0 or hand_values['Ace'] == 0:
+            return "Straight"
+        else:
+            return "Bust"
+    
+    # Identify the combo that we have received
+    combos = {
+        (5,0): "Five of a kind",
+        (4,1): "Four of a kind",
+        (3,2): "Full house",
+        (3,1): "Three of a kind",
+        (2,2): "Two pair",
+        (2,1): "One pair"
+    } 
+    return combos[combo]
+
+def remove_card(hand, card):
+    hand.pop(hand.index(card))
+
+def ask_for_keeps(hand, round):
+    cards_to_keep = []
+    while True:
+        try:
+            temp_hand = hand.copy()
+            cards_to_keep = input(f'Which dice do you want to keep for the {round} roll? ').split(" ")
+            
+            # Return empty list if they requst 'all' or 'All'
+            if cards_to_keep[0] == 'all' or cards_to_keep[0] == 'All':
+                return hand
+            
+            # Returns an empty hand if no input is provided
+            if cards_to_keep[0] == '':
+                return []
+            
+            for card in cards_to_keep:
+                if card not in temp_hand:
+                    raise ValueError
+                remove_card(temp_hand, card)
+            break
+            
+        except ValueError:
+            print('That is not possible, try again!')
+            
+    return cards_to_keep
+    
+    
+def play(simulate=False): 
+    
+    # Starting first round
+    hand = []
+    hand = generate_hand(hand)
+    hand_value = identify_hand(hand)
+    
+    # If we are running in simulate mode, simply return the result
+    if simulate:
+        return hand_value
+    
+    print('The roll is:', ' '.join(hand))
+    print('It is a', hand_value)
+    
+    # Starting Second round
+    hand = ask_for_keeps(hand, 'second')
+    if len(hand) == 5:
+        print('Ok, done.')
+        return
+    
+    hand = generate_hand(hand)
+    hand_value = identify_hand(hand)
+    
+    print('The roll is:', ' '.join(hand))
+    print('It is a', hand_value)
+    
+    # Starting Third Round
+    hand = ask_for_keeps(hand, 'third')
+    if len(hand) == 5:
+        print('Ok, done.')
+        return
+    
+    hand = generate_hand(hand)
+    hand_value = identify_hand(hand)
+    
+    print('The roll is:', ' '.join(hand))
+    print('It is a', hand_value)
 
 def simulate(n):
-    pass
-    # REPLACE PASS ABOVE WITH YOUR CODE
-
+    # print("***************************")
+    # print(f'poker_dice.simulate({n})')
+    hands = {
+        "Five of a kind": 0,
+        "Four of a kind": 0,
+        "Full house": 0,
+        "Straight": 0,
+        "Three of a kind": 0,
+        "Two pair": 0,
+        "One pair": 0,
+        # "Bust": 0 # We don't want to show busts
+    } 
     
-# DEFINE OTHER FUNCTIONS
+    for _ in range(n):
+        result = play(simulate=True)
+        if result != "Bust":
+            hands[result] += 1
+
+    for hand, count in hands.items():
+        print(f'{hand:15}: {(count/n*100):.2f}%')
